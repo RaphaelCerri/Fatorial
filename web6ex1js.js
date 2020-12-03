95e952f97d0344b847243f21ae3eacfb7e65c5d9
Raphael Cerri Caveagna <73002085+RaphaelCerri@users.noreply.github.com>
2020-12-03 04:44:17 -0300

    Add files via upload

---
+++ web6ex1js.js
@@ -0,0 +1,12 @@
+function fatorial() {
+    var numero = parseInt(document.getElementById('numero').value);
+    var resposta = document.getElementById('resposta');
+    var resultado = 1;
+    var count = 1;
+
+    while (count <= numero) {
+        resultado *= count;
+        count++;
+    }
+    document.write(resultado);
+}
